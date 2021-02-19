8c542e1bbed270aaaa6f2115e014745b4b7d5248
Mohamed Ali Ben Slimen <benslimenma@gmail.com>
2021-02-19 03:59:52 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import configureStore from "./store/configureStore";
+import configureStore, { persistor } from "./store/configureStore";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "./App.css";
@@ -11,23 +11,27 @@ import Categories from "./components/Categories";
 import Cart from "./components/Cart";
 import Products from "./components/Products";
 import CreateProduct from "./components/products/CreateProduct";
+import { PersistGate } from "redux-persist/integration/react";
 
 const store = configureStore();
+
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <NavBar />
-        <div className="container">
-          <Switch>
-            <Route path="/marques" component={Marques} />
-            <Route path="/categories" component={Categories} />
-            <Route path="/cart" component={Cart} />
-            <Route exact path="/products/new" component={CreateProduct} />
-            <Route exact path="/" component={Products} />
-          </Switch>
-        </div>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route path="/marques" component={Marques} />
+              <Route path="/categories" component={Categories} />
+              <Route path="/cart" component={Cart} />
+              <Route exact path="/products/new" component={CreateProduct} />
+              <Route exact path="/" component={Products} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   );
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,40 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducers/reducer";
 import api from "./middleware/api";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    api,
+  ],
+});
+
+export let persistor = persistStore(store);
 
 export default function () {
-  return configureStore({
-    reducer,
-    middleware: [...getDefaultMiddleware(), api],
-  });
+  return store;
 }
